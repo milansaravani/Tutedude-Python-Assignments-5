3a8d47fe291a84269637b3e110dbad90b597dac3
Milan Saravani <62232608+milansaravani@users.noreply.github.com>
2026-06-07 11:17:25 +0530

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,18 @@
+"""
+Problem Statement: Write a Python program that:
+1.   Creates a dictionary where student names are keys and their marks are values.
+2.   Asks the user to input a student's name.
+3.   Retrieves and displays the corresponding marks.
+4.   If the student’s name is not found, display an appropriate message.
+"""
+students = {
+    "Milan": 85,
+    "Satya": 72,
+    "Vrunda": 90,
+    "Nirali": 66
+}
+name = input("Enter the student's name: ")
+if name in students:
+    print(f"{name}'s marks: {students[name]}")
+else:
+    print("Student not found.")
